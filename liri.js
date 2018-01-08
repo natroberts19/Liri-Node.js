b57748b8f9ebf381808841d0b37d67f6b9a02acb
Natalie Roberts <nr_roberts@yahoo.com>
2018-01-07 21:13:11 -0500

    adding code to liri.js

--- liri.js
+++ liri.js
@@ -1,8 +1,71 @@
+// This code is necessary to read and set environment variables with the dotenv package:
 require("dotenv").config();
 
+// This code imports the keys.js file which holds the Twitter and Spotify API keys:
 var info = require("./keys.js");
 console.log(info);
 
-// var spotify = new Spotify(keys.spotify);
+// This allows access your keys information:
+var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 console.log(client);
+
+// Make it so liri.js can take in one of the following commands:
+// my-tweets, spotify-this-song, movie-this, do-what-it-says
+// CODE BELOW:
+// Set up a variable to represent each of the commands:
+var command = process.argv[2];
+
+// Set up a switch case for each command:
+switch(command) {
+    case "my-tweets": console.log();
+    break;
+    case "spotify-this-song": console.log();
+    break;
+    case "movie-this": console.log();
+    break;
+    case "do-what-it-says": console.log();
+    break;
+    default: console.log ("nothing");
+    break;
+}
+
+// Create command "node liri.js my-tweets" - This will show my last 20 tweets and when they were created in the terminal/bash window.
+
+// Create command "node liri.js spotify-this-song '<song name here>'" -  This will...
+
+// Create command "node liri.js movie-this '<movie name here>'" - pull in the info listed in the instructions and default to Mr. Nobody if no movie is entered.
+
+    // Include the request npm package.
+    var request = require("request");
+    // Store all of the arguments in an array. Use process.argv with no index position because the variable nodeArgs will only be called if it is greater than index position of 3.
+    var nodeArgs = process.argv;
+
+    // Create an empty variable for holding the movie name
+    var movieName = "";
+
+    // Loop through all the words in the node argument and handle the inclusion of "+"s to accomodate multi-word movie titles.
+    for (var i = 3; i < nodeArgs.length; i++) {
+        if (i > 3 && i < nodeArgs.length) {
+            movieName = movieName + "+" + nodeArgs[i];
+        } else {
+            movieName += nodeArgs[i];
+        }
+    }
+
+    // Then run a request to the OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
+
+    request(queryUrl, function (error, response, body) {
+
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            console.log("Release Year: " + JSON.parse(body).Year);
+        }
+    });
