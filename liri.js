8f83477c9047e540a4b6953b5d944265e8d0b578
Natalie Roberts <nr_roberts@yahoo.com>
2018-01-07 22:22:48 -0500

    added IMDB movie-this command code

--- liri.js
+++ liri.js
@@ -1,17 +1,18 @@
-// This code is necessary to read and set environment variables with the dotenv package:
-require("dotenv").config();
+// // This code is necessary to read and set environment variables with the dotenv package:
+// require("dotenv").config();
 
-// This code imports the keys.js file which holds the Twitter and Spotify API keys:
-var info = require("./keys.js");
-console.log(info);
+// // This code imports the keys.js file which holds the Twitter and Spotify API keys:
+// var info = require("./keys.js");
+// console.log(info);
 
-// This allows access your keys information:
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
-console.log(client);
+// // This allows access your keys information:
+// var spotify = new Spotify(keys.spotify);
+// var client = new Twitter(keys.twitter);
+// console.log(client);
 
 // Make it so liri.js can take in one of the following commands:
 // my-tweets, spotify-this-song, movie-this, do-what-it-says
+
 // CODE BELOW:
 // Set up a variable to represent each of the commands:
 var command = process.argv[2];
@@ -22,7 +23,7 @@ switch(command) {
     break;
     case "spotify-this-song": console.log();
     break;
-    case "movie-this": console.log();
+    case "movie-this": console.log("Movie Results: ");
     break;
     case "do-what-it-says": console.log();
     break;
@@ -40,32 +41,32 @@ switch(command) {
     var request = require("request");
     // Store all of the arguments in an array. Use process.argv with no index position because the variable nodeArgs will only be called if it is greater than index position of 3.
     var nodeArgs = process.argv;
-
     // Create an empty variable for holding the movie name
     var movieName = "";
-
     // Loop through all the words in the node argument and handle the inclusion of "+"s to accomodate multi-word movie titles.
     for (var i = 3; i < nodeArgs.length; i++) {
         if (i > 3 && i < nodeArgs.length) {
             movieName = movieName + "+" + nodeArgs[i];
+        // Add in a contingency for when no movie title is entered then default to Mr. Nobody:
+        // CODE HERE
         } else {
             movieName += nodeArgs[i];
         }
     }
-
-    // Then run a request to the OMDB API with the movie specified
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-    // This line is just to help us debug against the actual URL.
-    console.log(queryUrl);
+    // Then run a request to the OMDB API with the movie specified.
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy"
 
     request(queryUrl, function (error, response, body) {
-
         // If the request is successful
         if (!error && response.statusCode === 200) {
-
-            // Parse the body of the site and recover just the imdbRating
-            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-            console.log("Release Year: " + JSON.parse(body).Year);
+            // Parse the body of the site and recover the Title, Year, IMDB Rating, Rotten Tomatoes Rating, Country, Language, Plot, Actors.
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Year: " + JSON.parse(body).Year);
+            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("Country: " + JSON.parse(body).Country);
+            console.log("Language: " + JSON.parse(body).Language);
+            console.log("Plot: " + JSON.parse(body).Plot);
+            console.log("Actors: " + JSON.parse(body).Actors);
         }
     });
