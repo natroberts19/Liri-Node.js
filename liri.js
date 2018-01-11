e2a9a1a1344431886493cc2a0a8a47e15f4d22a3
Natalie Roberts <nr_roberts@yahoo.com>
2018-01-11 18:26:16 -0500

    added code for doIt()

--- liri.js
+++ liri.js
@@ -1,3 +1,36 @@
+// Create Inquirer menu to start the program:
+// Load the NPM Package inquirer
+// var inquirer = require("inquirer");
+
+// Create a "Prompt" with a series of questions.
+// inquirer
+//   .prompt([
+//     // Create a list of command choices for the user to pick from.
+//     {
+//         type: "list",
+//         message: "What would you like to find??",
+//         choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"],
+//         name: "commandChoice"
+//     },
+//     // Here we ask the user to confirm.
+//     {
+//       type: "confirm",
+//       message: "Are you sure:",
+//       name: "confirm",
+//       default: true
+//     }
+//   ])
+//   .then(function(inquirerResponse) {
+//     // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+//     if (inquirerResponse.confirm) {
+//       console.log("Press enter to run your search: " + inquirerResponse.commandChoice);
+//     }
+//     else {
+//       console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
+//     }
+//   });
+
+// Set up the environment to run the Spotify and Twitter API's:
 // This code is necessary to read and set environment variables with the dotenv package:
 require("dotenv").config();
 
@@ -6,6 +39,12 @@ var keys = require("./keys.js");
 
 // Now, make it so liri.js can take in the following commands: 
 // my-tweets, spotify-this-song, movie-this, do-what-it-says
+
+// Some globals:
+    // Create an empty variable for holding the song name
+    var songName = "";
+    // Create an empty variable for holding the movie name
+    var movieName = "";
 
 // COMMAND CODE BELOW:
 // Set up a variable to represent each of the commands:
@@ -27,7 +66,7 @@ switch (command) {
         doIt();
         break;
     default:
-        console.log("No command was entered.");
+        console.log("No command was entered. Please enter one of the following: 'my-tweets', 'spotify-this-song', 'movie-this', or 'do-what-it-says'");
         break;
 }
 
@@ -53,21 +92,23 @@ function twitter() {
                 console.log("---------------------------------");
                 console.log("natroberts19: ", tweets[i].text);
                 console.log("created at: ", tweets[i].created_at);
+                // Add tweets to log.txt file...
+                // fs.appendFile("log.txt", tweets[i].text);
             }
         }
     });
 }
 
 // SPOTIFY CODE: Command "node liri.js spotify-this-song '<song name here>'" - This will pull in the song info based on the instructions and default to "The Sign" by Ace of Base if no song is requested.
-function spotify() {
+function spotify(songName) {
     // Include the require Spotify package.
     var Spotify = require('node-spotify-api');
     //  Access the spotify keys from the keys.js file:
     var spotify = new Spotify(keys.spotify);
     // Store all of the arguments in an array. Use process.argv with no index position because the variable nodeArgs will only be called if it is greater than index position of 3.
     var nodeArgsSpotify = process.argv;
-    // Create an empty variable for holding the song name
-    var songName = "";
+    // // Create an empty variable for holding the song name
+    // var songName = "";
     // Loop through all the words in the node argument to accomodate multi-word song titles.
     for (var i = 3; i < nodeArgsSpotify.length; i++) {
         if (i > 3 && i < nodeArgsSpotify.length) {
@@ -91,52 +132,63 @@ function spotify() {
             console.log("Artist Name: ", data.tracks.items[0].album.artists[0].name);
             console.log("Album Name: ", data.tracks.items[0].album.name);
             console.log("Preview URL: ", data.tracks.items[0].preview_url)
+            console.log("---------------------------------");
         }
         // "else" conditional for if no song name is entered...add "The Sign" by Ace of Base.
+        // else if (!data) {
         // console.log("---------------------------------");
         // console.log("The Sign, Ace of Base");
+        // }        
     })
 };
 
 // MOVIES CODE: Command "node liri.js movie-this '<movie name here>'" - pull in the movie info listed in the instructions and default to Mr. Nobody if no movie is entered.
-function movies() {
-    // Include the request npm package.
-    var request = require("request");
-    // Store all of the arguments in an array. Use process.argv with no index position because the variable nodeArgsMovies will only be called if it is greater than index position of 3.
-    var nodeArgsMovies = process.argv;
-    // Create an empty variable for holding the movie name
-    var movieName = "";
-    // Loop through all the words in the node argument and handle the inclusion of "+"s to accomodate multi-word movie titles.
-    for (var i = 3; i < nodeArgsMovies.length; i++) {
-        if (i > 3 && i < nodeArgsMovies.length) {
-            movieName = movieName + "+" + nodeArgsMovies[i];
-        } else {
-            movieName += nodeArgsMovies[i];
+function movies(movieName) {
+    // If (!movieName) a movie is not entered, display Mr. Nobody info:
+    if (movieName === "") {
+        console.log("Mr. Nobody");
+    } else {
+        // Else, run the rest of the function:
+        // Include the request npm package.
+        var request = require("request");
+        // Store all of the arguments in an array. Use process.argv with no index position because the variable nodeArgsMovies will only be called if it is greater than index position of 3.
+        var nodeArgsMovies = process.argv;
+        // // Create an empty variable for holding the movie name
+        // var movieName = "";
+        // Loop through all the words in the node argument and handle the inclusion of "+"s to accomodate multi-word movie titles.
+        for (var i = 3; i < nodeArgsMovies.length; i++) {
+            if (i > 3 && i < nodeArgsMovies.length) {
+                movieName = movieName + "+" + nodeArgsMovies[i];
+            } else {
+                movieName += nodeArgsMovies[i];
+            }
         }
+        // Then run a request to the OMDB API with the movie specified.
+        var queryUrlMovies = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy"
+    
+        request(queryUrlMovies, function (error, response, body) {
+            // If the request is successful
+            if (!error && response.statusCode === 200) {
+                // Parse the body of the site and recover the Title, Year, IMDB Rating, Rotten Tomatoes Rating, Country, Language, Plot, Actors.
+                // Log the results:
+                console.log("---------------------------------")
+                console.log("Title: " + JSON.parse(body).Title);
+                console.log("Year: " + JSON.parse(body).Year);
+                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                console.log("Country: " + JSON.parse(body).Country);
+                console.log("Language: " + JSON.parse(body).Language);
+                console.log("Plot: " + JSON.parse(body).Plot);
+                console.log("Actors: " + JSON.parse(body).Actors);
+                console.log("---------------------------------");
+            } else {
+                // Add conditional if a movie name is not entered, default to "Mr. Nobody."
+                console.log("---------------------------------")
+                console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/");
+                console.log("---------------------------------");
+            }
+        });
     }
-    // Then run a request to the OMDB API with the movie specified.
-    var queryUrlMovies = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy"
-
-    request(queryUrlMovies, function (error, response, body) {
-        // If the request is successful
-        if (!error && response.statusCode === 200) {
-            // Parse the body of the site and recover the Title, Year, IMDB Rating, Rotten Tomatoes Rating, Country, Language, Plot, Actors.
-            // Log the results:
-            console.log("---------------------------------")
-            console.log("Title: " + JSON.parse(body).Title);
-            console.log("Year: " + JSON.parse(body).Year);
-            console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-            console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-            console.log("Country: " + JSON.parse(body).Country);
-            console.log("Language: " + JSON.parse(body).Language);
-            console.log("Plot: " + JSON.parse(body).Plot);
-            console.log("Actors: " + JSON.parse(body).Actors);
-        } else {
-            // Add conditional if a movie name is not entered, default to "Mr. Nobody."
-            console.log("---------------------------------")
-            console.log("If you haven't watched Mr. Nobody, then you should: http://www.imdb.com/title/tt0485947/");
-        }
-    });
 }
 
 // DOIT CODE: Command "node liri.js do-what-it-says" - using fs node package, read the info listed from the random.txt file.
@@ -170,22 +222,7 @@ function doIt() {
         var song = dataArr[1];
         console.log("Song: ", song);
 
-        // Not working!??
-        function songSearch() {
-            spotify.search({
-                type: 'track',
-                query: song
-            }, function (err, data) {
-                if (err) {
-                    return console.log('Error occurred: ' + err);
-                } else if (err, data) {
-                    console.log("---------------------------------");
-                    console.log("Song Name: ", song);
-                    console.log("Artist Name: ", data.tracks.items[0].album.artists[0].name);
-                    console.log("Album Name: ", data.tracks.items[0].album.name);
-                    console.log("Preview URL: ", data.tracks.items[0].preview_url)
-                }
-            })
-        }
+        // Now try to search for the song at dataArr position 1 and display the information.??
+        spotify(song);
     });
 }
